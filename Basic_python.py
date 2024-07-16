2e2500c4d6b09759bb76bb7cfc90e446b1cbd01d
Masaharu41 <dalekrebel@gmail.com>
2024-07-15 18:54:13 -0700

    If Broken But Math Works

---
+++ Basic_python.py
@@ -0,0 +1,15 @@
+# File: apply example
+#TODO
+#{} Print to terminal
+#{} Create user selections
+#{} Use basic math
+#{} create a basic calculator script
+import sys
+import os
+
+print("Welcome to Math")
+sys.stdout.write('Line 2 to stdout')
+sys.stdout.flush()
+os.write(1,b'Line 3 to stdout')
+
+
