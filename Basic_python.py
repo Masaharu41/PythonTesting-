3fbc2611fded3f06cff40996dc57403ca2173579
Masaharu41 <dalekrebel@gmail.com>
2024-07-17 21:28:43 -0700

    Updated TODO and Began Adding New Functions

--- Basic_python.py
+++ Basic_python.py
@@ -4,7 +4,8 @@
 #{*} Create user selections
 #{*} Use basic math
 #{*} Allow Multiple calcuations without closing script
-#{} create a basic calculator script
+#{*} create a basic calculator script
+#{} create a calculator script that can save the last answer to use on the next iteration
 import sys
 import os
 
@@ -22,6 +23,46 @@ while True:
     #check user input for q to leave while loop
     if userinput1 == "q":
             break
+    elif userinput1 == "r":
+        try:
+              #Check that the userinput is an integer
+            int(userinput1)
+            print ("Enter the second number")
+            userinput2 = sys.stdin.readline().strip(
+
+            ) 
+        
+        except:
+            print("Sorry you must enter a valid integer")
+            
+        if userinput2 == "q":
+           break
+        else:
+            try:
+                int(userinput2)
+                print("Please select operation + for addition - for substration * for multiplicationn / for division or Q to quit" )
+                userop = sys.stdin.readline().strip(
+
+                    )
+                print(userop)
+                    # Note the input from the terminal is a string data type. must force to integer to proper
+                    # compute the operation
+                if userop == "+":
+                    temp = int(usersave) + int(userinput2)
+                elif userop == "-":
+                    temp = int(usersave) - int(userinput2)
+                elif userop == "*":
+                    temp = int(usersave) * int(userinput2)
+                elif userop == "/":
+                    temp = (int(usersave) / int(userinput2)) # "The remainder is" + (int(userinput1) % int(userinput2)) 
+                elif userop == "q":
+                    break
+                else:
+                    temp = "Caclulation Failed"
+                print(temp)
+                usersave = temp 
+            except:
+                print("An Error has occured")
     else:
         try:
             #Check that the userinput is an integer
@@ -59,6 +100,7 @@ while True:
                 else:
                     temp = "Caclulation Failed"
                 print(temp)
+                usersave = temp 
             except:
                 print("Sorry you must enter a valid integer")
             
