fc7bc6f7c4867dbe85cd4fcf536f56f474c6d3c7
Masaharu41 <dalekrebel@gmail.com>
2024-08-16 15:12:41 -0600

    Basic Gui App with Comments

--- GUI App/Grocery Gui.py
+++ GUI App/Grocery Gui.py
@@ -1,6 +1,25 @@
+# Import Tkinter Gui modules and make proper reference
+#
 import tkinter
 from tkinter import *
-m = tkinter.Tk()
-w = Label(m, text='Nice to Meet You')
-w.pack()
-m.mainloop()
+# root is the primary window variable which is spawned by Tkinter
+root = Tk()
+# spawns a title for the window and sets dimensions 
+root.title("Welcome Owen")
+# Set Geometry(width x height)
+root.geometry('500x400')
+#label for the root window
+lbl = Label(root, text = "have fun friends")
+lbl.grid()
+# Define behaviour when the button is clicked
+def clicked():
+    lbl.configure(text="Why Hello There!")
+#
+#defines the button, text, and the color of the text
+btn = Button(root, text="click here",
+            fg = "red", command=clicked)
+#Configures how many buttons will be spawned
+btn.grid(column=1, row=0)
+#
+#
+root.mainloop()
